e3c13ca302f8892de7363bc33c17cd666593e696
ShibiDhurgaM <137518782+ShibiDhurgaM@users.noreply.github.com>
2024-07-08 13:22:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,12 +10,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const mongoURI = process.env.MONGO_URL;
 
-const corsOptions = {
-  origin: 'https://flight-website-frontend.vercel.app',
-  optionsSuccessStatus: 200,
-};
+// const corsOptions = {
+//   origin: 'https://flight-website-frontend.vercel.app',
+//   optionsSuccessStatus: 200,
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 mongoose.connect(mongoURI, {
